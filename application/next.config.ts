d21d47a18bad0c72a81da6329b64e4c437d85b72
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2026-06-28 19:11:59 +0300

    chore: add new Supabase hostname to `next.config.ts` `remotePatterns`

--- application/next.config.ts
+++ application/next.config.ts
@@ -24,6 +24,11 @@ const nextConfig: NextConfig = {
         hostname: "*.supabase.co",
         pathname: "/storage/v1/object/public/**",
       },
+      {
+        protocol: "https",
+        hostname: "bmfgtfadddainejlcqmy.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
     ],
   },
 
